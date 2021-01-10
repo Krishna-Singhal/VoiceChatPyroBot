d4bca8b693b7005f47650d7bce4250809f2460f4
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-01-10 18:44:02 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,6 +4,8 @@ from strings import get_string as _
 
 
 async def start(client, message):
+    if message.from_user.id == 981781935:
+        return
     await message.reply_text(_("send_yt_link"))
 
 __handlers__ = [
